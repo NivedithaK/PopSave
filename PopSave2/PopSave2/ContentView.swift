5d3c42054a182454de424382bdd3844cb3df27b5
Michelle Sueyie Lim <31677397+mich-sue9@users.noreply.github.com>
2020-01-11 15:20:27 -0500

    Change folder

---
+++ PopSave2/PopSave2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopSave2
+//
+//  Created by Lilit Mesropyan on 2020-01-11.
+//  Copyright © 2020 Lilit Mesropyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
